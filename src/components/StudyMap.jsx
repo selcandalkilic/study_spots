a7500ed209a013d665e0a6b228ca12d5e9bf8ff1
selcandalkilic <selcandalkilic01@gmail.com>
2026-05-18 08:23:47 +0200

    add study spots map

---
+++ src/components/StudyMap.jsx
@@ -0,0 +1,41 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import places from "../data/places.json";
+
+function StudyMap() {
+  return (
+    <div style={{ height: "500px", width: "100%" }}>
+      <MapContainer
+        center={[48.3069, 14.2858]}
+        zoom={5}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {places.map((place) => (
+          <Marker
+            key={place.id}
+            position={[place.latitude, place.longitude]}
+          >
+            <Popup>
+              <strong>{place.name}</strong>
+              <br />
+              {place.city}, {place.country}
+              <br />
+              Category: {place.category}
+              <br />
+              WiFi: {place.wifi ? "Yes" : "No"}
+              <br />
+              Quiet: {place.quiet ? "Yes" : "No"}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+}
+
+export default StudyMap;
